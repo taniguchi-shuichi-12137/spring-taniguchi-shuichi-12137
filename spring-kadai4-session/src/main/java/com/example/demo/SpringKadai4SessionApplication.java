92f929edc4887ae463e4cc1dbc18ce16ff62663f
taniguchi-shuichi-12137 <718-student012@la-study.com>
2026-05-07 16:28:08 +0900

    5/7セッションSP40

---
+++ spring-kadai4-session/src/main/java/com/example/demo/SpringKadai4SessionApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKadai4SessionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKadai4SessionApplication.class, args);
+	}
+
+}
