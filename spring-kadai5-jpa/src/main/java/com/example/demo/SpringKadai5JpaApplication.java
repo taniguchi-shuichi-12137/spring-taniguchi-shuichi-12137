024d66c8ae2eddab3cdcf10c846b042da59a13ae
taniguchi-shuichi-12137 <718-student012@la-study.com>
2026-05-08 16:38:26 +0900

    5/8DB連携SP50

---
+++ spring-kadai5-jpa/src/main/java/com/example/demo/SpringKadai5JpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKadai5JpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKadai5JpaApplication.class, args);
+	}
+
+}
